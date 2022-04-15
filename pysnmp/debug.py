74466ce2713914564be8551767457a0c2a05680b
LissaGreense <walissa13@gmail.com>
2022-04-15 11:48:45 +0200

    Fixed hex printing in debug mode

--- pysnmp/debug.py
+++ pysnmp/debug.py
@@ -143,5 +143,5 @@ def setLogger(l):
 
 def hexdump(octets):
     return ' '.join(
-        '%s%.2X' % (n % 16 == 0 and ('\n%.5d: ' % n) or '', x)
-        for n, x in zip(range(len(octets)), octs2ints(octets)))
+        ['{}{:02X}'.format(n % 16 == 0 and ('\n%.5d: ' % n) or '', x) for n, x in zip(range(len(octets)),
+                                                                                      octs2ints(octets))])
