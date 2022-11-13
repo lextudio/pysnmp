7f9f04f74a1cd3eb0048cbeda9232eff724947e8
Lex Li <support@lextm.com>
2022-11-13 00:00:46 -0500

    Published test lextudio package.

--- pysnmp/__init__.py
+++ pysnmp/__init__.py
@@ -1,7 +1,5 @@
 # http://www.python.org/dev/peps/pep-0396/
-__version__ = '5.0.24'
-# another variable is required to prevent semantic release from updating version in more than one place
-main_version = __version__
+__version__ = '5.0.18'
 # backward compatibility
 # for beta versions, integer casting throws an exception, so string part must be cut off
 if 'beta' in __version__:
