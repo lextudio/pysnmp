d33ceed015e6ac90d027de9fa2859a52b2d30a8e
Lex Li <support@lextm.com>
2024-11-04 19:53:15 -0500

    Reverted some fixed length related changes.

--- pysnmp/proto/rfc1902.py
+++ pysnmp/proto/rfc1902.py
@@ -234,20 +234,20 @@ class OctetString(univ.OctetString):
     # having zero-range size constraint applied. The following is
     # supposed to be used for setting and querying this property.
 
-    fixed_length = None
+    fixedLength = None
 
     def set_fixed_length(self, value):
         """Set fixed length."""
-        self.fixed_length = value
+        self.fixedLength = value
         return self
 
     def is_fixed_length(self):
         """Return if fixed length."""
-        return self.fixed_length is not None
+        return self.fixedLength is not None
 
     def get_fixed_length(self):
         """Return fixed length."""
-        return self.fixed_length
+        return self.fixedLength
 
     def clone(self, *args, **kwargs):
         """Clone the data."""
@@ -367,7 +367,7 @@ class IpAddress(OctetString):
     subtypeSpec = OctetString.subtypeSpec + constraint.ValueSizeConstraint(
         4, 4
     )  # noqa: N815
-    fixed_length = 4
+    fixedLength = 4
 
     def prettyIn(self, value):  # noqa: N802
         """Convert string to IP address."""
