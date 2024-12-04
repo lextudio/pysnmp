e7b7f80b0fd98154ece2ec0e5a42ea50811db7ad
Lex Li <support@lextm.com>
2024-12-03 21:29:12 -0500

    Bumped the version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -74,7 +74,7 @@ author = "LeXtudio Inc. <support@lextudio.com>"
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = "7.1.13"
+release = "7.1.14"
 # The short X.Y version.
 version = ".".join(release.split(".")[:2])
 
